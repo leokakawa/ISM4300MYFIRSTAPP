2412dba6ee071b2d3e7cbbd3808daa3e484ccccc
leonardo <leonardo@LK>
2019-08-29 16:36:15 -0400

    This is my first app

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ISM4300MYFIRSTAPP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is my first Console App");
+            Console.WriteLine("");
+            Console.WriteLine("I am learning how to publish GitHub");
+            Console.ReadKey();
+        }
+    }
+}
